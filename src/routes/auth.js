4325c43b1e7c295213bf649fbe210479ae65b835
allan-paz <allan.paz@laureate.net>
2024-03-19 19:09:58 -0600

    added token verify middleware to images routes.

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,10 @@
 const { signIn, signUp, forgotPassword, resetPassword } = require('../controllers/v1/auth');
-const { jwtVerification, resetPasswordverifyToken } = require('../middlewares/jwtAuth');
+const { resetPasswordverifyToken } = require('../middlewares/jwtAuth');
 module.exports = (router) => {
 
     router.get('/signIn', signIn);
     router.post('/signUp', signUp);
     router.get('/forgotPassword', forgotPassword);
     router.put('/resetPassword', resetPasswordverifyToken, resetPassword);
+    
 };
