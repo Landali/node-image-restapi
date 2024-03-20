8c37a590ca0f1b0bc0cdb4cb11d84d3d9f10d8b6
allan-paz <allan.paz@laureate.net>
2024-03-19 20:47:45 -0600

    add unsplash api search param validator.

--- src/controllers/v1/images.js
+++ src/controllers/v1/images.js
@@ -1,10 +1,13 @@
-const { searchImage } = require('../../services/unsplashApi')
-
+const { searchImage } = require('../../services/unsplashApi');
+const { paramsValidator } = require('../../utils/validators/unsplashApi');
 
 module.exports = {
     async getImages(req, res) {
         console.log('Saving images', req.body);
-        const images = await searchImage(req.body);
+
+        const params = paramsValidator(req.body);
+        const images = await searchImage(params);
+        
         return res.send({ Status: "Success", data: { ...images.data } })
     }
 }
