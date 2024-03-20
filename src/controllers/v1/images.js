8c47f08229d0e8348b51875ea85be206adb2299c
allan-paz <allan.paz@laureate.net>
2024-03-19 23:36:05 -0600

    add saveImage controller to save image endpoint

--- src/controllers/v1/images.js
+++ src/controllers/v1/images.js
@@ -3,11 +3,15 @@ const { paramsValidator } = require('../../utils/validators/unsplashApi');
 
 module.exports = {
     async getImages(req, res) {
-        console.log('Saving images', req.body);
+        console.log('Get images');
 
         const params = paramsValidator(req.body);
         const images = await searchImage(params);
-        
+
         return res.send({ Status: "Success", data: { ...images.data } })
+    },
+    async saveImage(req, res) {
+        console.log('Save Image');
+        return res.send({ Status: "Success" })
     }
 }
--- src/routes/images.js
+++ src/routes/images.js
@@ -1,10 +1,7 @@
 const { jwtVerification } = require('../middlewares/jwtAuth');
-const { getImages } = require('../controllers/v1/images');
+const { getImages, saveImage } = require('../controllers/v1/images');
 module.exports = (router) => {
     router.get('/getImages', jwtVerification, getImages)
 
-    router.post('/saveImage', jwtVerification, (req, res) => {
-        console.log('Saving images');
-        return res.send({ Status: "Success" })
-    })
+    router.post('/saveImage', jwtVerification, saveImage)
 };
