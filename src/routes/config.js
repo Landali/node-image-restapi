57f0bafcd5b64f816d53dab6fa30ec9124ff2a69
allan-paz <allan.paz@laureate.net>
2024-03-21 06:04:09 -0600

    add token middleware to configuration endpoint.

--- src/routes/config.js
+++ src/routes/config.js
@@ -1,7 +1,7 @@
 const { saveConfigsToStore } = require('../controllers/v1/config');
-
+const { jwtVerification } = require('../middlewares/jwtAuth');
 module.exports = (router) => {
 
-    router.get('/setupConfig', saveConfigsToStore);
+    router.get('/setupConfig', jwtVerification, saveConfigsToStore);
 
 };
